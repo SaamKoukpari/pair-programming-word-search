7093482f870031a1aa35bb1a3ff24f41780d17dd
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-22 19:04:13 -0700

    initial commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -23,7 +23,7 @@ describe("#wordSearch()", function() {
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], // <-- INDEX 1
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
@@ -35,4 +35,62 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['T', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['O', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], 
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['M', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['Y', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TOMMY')
+  
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['T', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['O', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], 
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['M', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['Y', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SAAM')
+  
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['T', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['0', 'E', 'I', 'N', 'F', 'E', 'L', 'D'], 
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['M', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['Y', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TOMMY')
+  
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['T', 'O', 'M', 'M'],
+      ['S', 'A', 'M', 'M']
+    ], 'OA')
+  
+    assert.isTrue(result);
+  });
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,70 @@
+// ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'], 
+// ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+// ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+// ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+// ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+// ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+// ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+// ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+// ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    // Horizontal check of each index of the original array
+    for (line of horizontalJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
+    } 
+
+    // Turn columns into rows of original array
+    let transposedLetters = transpose(letters);
+    // [                        
+    //  [ 'T', 'O', 'M ]       [ ['T', 'S],  => [ 'TS'       ]
+    //  [ 'S', 'A', "M"]   =>    ['O', 'A],       'OA'
+    // ]                         ['M', 'M] ]      'MM'
+
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+
+    // Horizontal check of verticalJoin
+    for (line of verticalJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
+    } 
+
+    return false;
 }
 
-module.exports = wordSearch
+// Returns a transposed version of the passed 2-dimensional array.
+  // [                        
+    //  [ 'T', 'O', 'M ]       [ ['T', 'S]  
+    //  [ 'S', 'A', "M"]   =>    ['O', 'A]
+    // ]                         ['M', 'M] ]
+const transpose = (letterVert) => {
+    let transposedLilVert = [];
+
+    for (let i = 0; i < letterVert[0].length; i++) {
+        let transposedColumn = [];
+
+        for (let j = 0; j < letterVert.length; j++) {
+            transposedColumn.push(letterVert[j][i]);
+        }
+        
+        transposedLilVert.push(transposedColumn);
+    }
+
+    // for (const row of letVert) {
+    //     for (const col of row) {
+    //         process.stdout.write(col + " ");
+    //     }
+    //     process.stdout.write('\n')
+    // }
+
+    // console.log(transpose)
+    
+    return transposedLilVert;
+}
+
+module.exports = wordSearch;
